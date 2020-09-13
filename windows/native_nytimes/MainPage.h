8db111153680dc54d9416a03add375f2dd37b417
Ahmed <ahmedali.96@hotmail.com>
2020-09-12 22:45:36 -0400

    name change

---
+++ windows/native_nytimes/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::native_nytimes::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::native_nytimes::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
